78a21a4d277c79ed9b0de16141d3714514a28fb0
Jhalakupadhyay <jhalak92upadhyay@gmail.com>
2024-09-22 17:40:59 +0530

    feat:added the fixed animation.

--- lib/badge_animation/ani_fixed.dart
+++ lib/badge_animation/ani_fixed.dart
@@ -1,23 +1,21 @@
 import 'package:badgemagic/badge_animation/animation_abstract.dart';
 
-class UpAnimation extends BadgeAnimation {
+class FixedAnimation extends BadgeAnimation {
   @override
   void processAnimation(int badgeHeight, int badgeWidth, int animationIndex,
       List<List<bool>> processGrid, List<List<bool>> canvas) {
     int newWidth = processGrid[0].length;
     int newHeight = processGrid.length;
-    int animationValue = animationIndex ~/ ((newWidth / badgeHeight));
+    int horizontalOffset = (badgeWidth - newWidth) ~/ 2;
 
     for (int i = 0; i < badgeHeight; i++) {
       for (int j = 0; j < badgeWidth; j++) {
-        if (j < badgeWidth && i < badgeHeight) {
-          bool upCondition = (i >= 0 &&
-              i < newHeight &&
-              j >= 0 &&
-              j < newWidth &&
-              processGrid[(i + animationValue + newHeight) % newHeight][j]);
-
-          canvas[i][j] = upCondition;
+        int sourceCol = j - horizontalOffset;
+        bool isWithinNewGrid = 
+            sourceCol >= 0 &&
+            sourceCol < newWidth;
+        if (isWithinNewGrid) {
+          canvas[i][j] = processGrid[i][sourceCol];
         }
       }
     }
